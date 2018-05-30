361318c6ba123cf2f1180936cc6cec66cd9ab594
heinrich-boll <rezaie2114@gmail.com>
2018-05-30 18:00:28 +0430

    project 'absenteeism_at_work' wich picked up from archive.ics.uci.edu/ml/datasets

--- Classification:Absenteeism_at_work.py
+++ Classification:Absenteeism_at_work.py
@@ -11,7 +11,7 @@ import keras
 from keras import regularizers
 from keras.utils import to_categorical
 from numpy import array
-
+from scipy.stats import zscore
 
 
 
@@ -64,46 +64,39 @@ def to_xy(df, target):
 
 
 
-def to_one_hot(df , target):
-    # find out the type of the target column.  Is it really this hard? :(
-    target_type = df[target].dtypes
-    target_type = target_type[0] if hasattr(target_type, '__iter__') else target_type
-    # Encode to int for classification, float otherwise. TensorFlow likes 32 bits.
-    if target_type in (np.int64, np.int32):
-        # Classification
-        dummies = pd.get_dummies(df[target])
-        return  dummies.as_matrix().astype(np.float32)
-    else:
-        # Regression
-        return  df.as_matrix([target]).astype(np.float32)
 
 
 
 
+df = pd.read_csv('test_df.csv',na_values=['NA','?'])
+
+df['Work load Average/day ']=zscore(df['Work load Average/day '])
+df['Transportation ']=zscore(df['Transportation '])
+df['Distance']=zscore(df['Distance'])
+df['Service time']=zscore(df['Service time'])
+df['Age']=zscore(df['Age'])
+df['Hit target']=zscore(df['Hit target'])
+df['Weight']=zscore(df['Weight'])
+df['Height']=zscore(df['Height'])
+df['Absenteeism time in hours']=zscore(df['Absenteeism time in hours'])
+df['Body mass index']=zscore(df['Body mass index'])
+df['Son']=zscore(df['Son'])
+df['Pet']=zscore(df['Pet'])
 
 
 
-
-df = pd.read_csv('Absenteeism_at_work.csv',na_values=['NA','?'])
-id=df['ID']
+# id=df['ID']
 # df['Work load Average per day '] =df['Work load Average per day ']/df['Work load Average per day '].mean()
-df.drop('ID',axis=1,inplace=True)
-
-dummies = pd.get_dummies(df['Reason for absence'])
-df['Reason for absence'] = dummies.as_matrix().astype(np.float32)
+# df.drop('ID',axis=1,inplace=True)
+# df = pd.get_dummies(df,columns=['Month of absence',
+#                                'Seasons','Day of the week','Disciplinary failure',
+#                                 'Education'] , drop_first= True )
+df.to_csv('Absenteeism_at_work.csv',index=False)
 
 x,y = to_xy(df,'Reason for absence')
 
 
-print(x[: , 0])
 
-
-
-# x['Month of absence'] = keras.utils.to_categorical(x['Month of absence'], 12)
-# x['Day of the week'] = keras.utils.to_categorical(x['Day of the week'], 7)
-# x['Seasons'] = keras.utils.to_categorical(y['Seasons'], 4)
-
-# y = keras.utils.to_categorical(y, y.shape[1])
 
 
 
@@ -116,13 +109,16 @@ x_train, x_test, y_train, y_test = train_test_split(
 print(y.shape[1])
 model = Sequential([
 
-    Dense(500, input_dim=x.shape[1], kernel_initializer='normal', activation='relu'),
+
+    Dense(100, input_dim=x.shape[1], kernel_initializer='normal', activation='tanh'),
+    Dense(50, input_dim=x.shape[1], kernel_initializer='normal', activation='tanh'),
+
 
     Dense(y_train.shape[1], kernel_initializer='normal',activation='softmax')
 
 
 ])
-model.compile(loss='categorical_crossentropy', optimizer=Adam(0.001))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
 
 monitor = EarlyStopping(monitor='val_loss', min_delta=1e-3, patience=5, verbose=1, mode='auto')
 
@@ -140,6 +136,8 @@ print(log_loss(y_test,pred))
 predict_classes = np.argmax(pred,axis=1)
 expected_classes = np.argmax(y_test,axis=1)
 
-print("Predictions: {}".format(predict_classes[0:10]))
-print("Expected:    {}".format(expected_classes[0:10]))
+print("Predictions: {}".format(predict_classes[0:25]))
+print("Expected:    {}".format(expected_classes[0:25]))
 
+
+model.save('Absenteeism_at_work.h5')
